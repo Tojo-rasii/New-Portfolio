db93fe756e6b334f6817ecc2bbbe9c5e42f09665
Tojo-rasii <tojorasii1619@gmail.com>
2024-10-15 13:06:47 +0300

    suc

--- mobile.js
+++ mobile.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const moon = document.querySelector('.bi-moon-stars');
     const sun = document.querySelector('.bi-sun');
+    const sectionAfter = document.querySelector('#headerMobile section::after');
+    const a = document.querySelectorAll('#headerMobile a');
     const mobileArticle = document.querySelector('#headerMobile section article:last-child');
     const after = document.querySelector('.after');
     const show = document.getElementById('i-list');
@@ -11,13 +13,23 @@ document.addEventListener('DOMContentLoaded', () => {
     show.addEventListener('click', () => {
 
         mobileArticle.classList.add('display');
+        sectionAfter.style.display = 'block';
 
     })
 
+
     close.addEventListener('click', () => {
-
-
         mobileArticle.classList.remove('display');
+        sectionAfter.style.display = 'none';
+
+    })
+
+
+    a.forEach((value) => {
+        value.addEventListener('click', () => {
+            mobileArticle.classList.remove('display');
+
+        })
     })
 })
 
--- project.js
+++ project.js
@@ -1,103 +1,32 @@
-const list = document.getElementById('listProject');
-const prevPage = document.getElementById('prevPage');
-const nextPage = document.getElementById('nextPage');
+const carousel = document.querySelector('.carousel');
+const arrowsBtn = document.querySelectorAll('.wrapper i');
+const firstCardWidth = carousel.querySelector('.card').offsetWidth;
+let isDragging = false, startX, startScrollLeft;
 
-const prevPageMob = document.getElementById('prevPageMob');
-const nextPageMob = document.getElementById('nextPageMob');
+arrowsBtn.forEach(value => {
+  value.addEventListener('click', () => {
+    carousel.scrollLeft += value.id === 'left' ? -firstCardWidth : firstCardWidth;
 
-// Les articles à afficher
-const listArticle = [
-  {
-    img: 'https://tojo-rasii.github.io/rasii-online/static/media/portfolio%20darkmode.453289df70dec5aa9e9c.png',
-    title: 'Mon portfolio',
-    desc: 'Mon portfolio est une expérience interactive e...',
-    techno: '#langage'
-  },
-  {
-    img: 'https://tojo-rasii.github.io/rasii-online/static/media/portfolio%20darkmode.453289df70dec5aa9e9c.png',
-    title: 'Mon portfolio',
-    desc: 'Mon portfolio est une expérience interactive e...',
-    techno: '#langage'
-  },
-  {
-    img: 'https://tojo-rasii.github.io/rasii-online/static/media/portfolio%20darkmode.453289df70dec5aa9e9c.png',
-    title: 'Mon portfolio',
-    desc: 'Mon portfolio est une expérience interactive e...',
-    techno: '#langage'
-  },
-  {
-    img: 'https://tojo-rasii.github.io/rasii-online/static/media/portfolio%20darkmode.453289df70dec5aa9e9c.png',
-    title: 'Mon portfolio',
-    desc: 'Mon portfolio est une expérience interactive e...',
-    techno: '#langage',
-  },
-  // Ajouter d'autres articles ici
-];
+  })
+})
 
-// Paramètres de pagination
-const itemsPerPage = 3; // Nombre d'articles par page
-let currentPage = 1;
-const totalPages = Math.ceil(listArticle.length / itemsPerPage);
-
-// Fonction pour afficher les articles de la page actuelle
-function displayArticles(page) {
-  list.innerHTML = ''; // Effacer le contenu précédent
-  const start = (page - 1) * itemsPerPage;
-  const end = start + itemsPerPage;
-
-  const articlesToDisplay = listArticle.slice(start, end);
-
-  articlesToDisplay.forEach((lists) => {
-    list.innerHTML += `
-      <picture id='pic'>
-        <figure>
-          <img src=${lists.img} alt="image">
-        </figure>
-        <figcaption>
-          <p>${lists.title}</p>
-          <p>${lists.desc}</p>
-          <p><span>${lists.techno}</span></p>
-        </figcaption>
-      </picture>
-    `;
-  });
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add('dragging');
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
 }
 
-// Écouteur pour le bouton "page suivante"
-nextPage.addEventListener('click', () => {
-  if (currentPage < totalPages) {
-    currentPage++;
-    displayArticles(currentPage);
-  }
-});
 
-// Écouteur pour le bouton "page précédente"
-prevPage.addEventListener('click', () => {
-  if (currentPage > 1) {
-    currentPage--;
-    displayArticles(currentPage);
-  }
-});
-// Écouteur pour le bouton "page suivante"
-nextPageMob.addEventListener('click', () => {
-  if (currentPage < totalPages) {
-    currentPage++;
-    displayArticles(currentPage);
-  }
-  window.location.href = '#Projets'
+const dragg = (e) => {
+  if (!isDragging) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
 
-
-});
-
-// Écouteur pour le bouton "page précédente"
-prevPageMob.addEventListener('click', () => {
-  if (currentPage > 1) {
-    currentPage--;
-    displayArticles(currentPage);
-  }
-  window.location.href = '#Projets'
-
-});
-
-// Afficher la première page lors du chargement
-displayArticles(currentPage);
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove('dragging');
+}
+carousel.addEventListener('mousedown', dragStart)
+carousel.addEventListener('mousemove', dragg)
+carousel.addEventListener('mouseup', dragStop)
